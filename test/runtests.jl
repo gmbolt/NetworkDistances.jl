df434635ab9d069a2941d2725061747603fe7317
George Bolt <g.bolt@lancaster.ac.uk>
2023-03-20 21:39:09 +0000

    Try adding realtive path

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 @testset "NetworkDistances.jl" begin
 
-    include("distances/multisets/matching_distances_test.jl")
+    include("./distances/multisets/matching_distances_test.jl")
 
 
 end
