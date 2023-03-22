f4498d339a61388226b4352b2d758ce9d0851015
George Bolt <g.bolt@lancaster.ac.uk>
2023-03-22 20:56:16 +0000

    Remove old comments

--- src/distances/multisets/matching_distances_eval.jl
+++ src/distances/multisets/matching_distances_eval.jl
@@ -16,12 +16,6 @@ function (d::T where {T<:CompleteMatchingDistance})(
 ) where {S}
     C = get_cost_matrix_dynamic(d, X, Y)
     return eval_distance(get_optimiser(d), C)
-    # # x = ones(size(C, 1))
-    # # out = PythonOT.emd2(
-    # #     x, x, C
-    # # )
-    # assignment, cost = hungarian(C)
-    # return cost
 end
 
 function Base.show(io::IO, d_gen::General{T}) where {T<:CompleteMatchingDistance}
