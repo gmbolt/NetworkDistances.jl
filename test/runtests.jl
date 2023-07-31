1e33feb4adc1299cf7564de326feb9c0721ef776
George Bolt <g.bolt@lancaster.ac.uk>
2023-07-31 18:20:54 +0100

    Update CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test, NetworkDistances
 
 @testset "NetworkDistances.jl" begin
 
-
+    println(readdir())
     include("matching_distances_test.jl")
     include("distances/multisets/matching_distances_test.jl")
 
