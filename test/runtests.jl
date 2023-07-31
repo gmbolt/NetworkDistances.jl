4edf14653c15562a3c17304d2fe8ac20d0ee0d29
George Bolt <g.bolt@lancaster.ac.uk>
2023-07-31 16:52:04 +0100

    Update testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
-using NetworkDistances
-using Test
+using Test, NetworkDistances
+
 
 @testset "NetworkDistances.jl" begin
 
 
+    include("matching_distances_test.jl")
     include("distances/multisets/matching_distances_test.jl")
 
 
