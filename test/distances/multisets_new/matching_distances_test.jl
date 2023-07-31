370ec31d5b01afec47347aeb2e4ce24b6b0e67a7
George Bolt <g.bolt@lancaster.ac.uk>
2023-07-31 18:37:07 +0100

    Refactor

--- test/distances/multisets_new/matching_distances_test.jl
+++ test/distances/multisets_new/matching_distances_test.jl
@@ -8,16 +8,14 @@
     dists = [
         MatchingDistance(LCS()),
         FastMatchDist(LCS(), 2n),
-        FixPenMatchDist(LCS(), 1.0)
-
-    ]
+        FixPenMatchDist(LCS(), 1.0)]
     # Match and FastMatch should be equal
-    @test dists[1](x,y) == dists[2](x,y)  
+    @test dists[1](x, y) == dists[2](x, y)
 
     # Test properties of distances 
-    for d in dists 
-        @test d(x,x) == 0.0
-        @test d(x,y) == d(y,x)
+    for d in dists
+        @test d(x, x) == 0.0
+        @test d(x, y) == d(y, x)
         @test d(nothing, nothing) == 0.0
-    end 
+    end
 end
