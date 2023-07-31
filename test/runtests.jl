8b218ed839c7847faaec47e42bf26c0fbb0c521c
George Bolt <g.bolt@lancaster.ac.uk>
2023-07-31 18:53:17 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,7 @@ using Test, NetworkDistances
 
 @testset "NetworkDistances.jl" begin
 
-    println(readdir())
-    println(readdir("distances/"))
-    include("matching_distances_test.jl")
-    include("distances/multisets_new/matching_distances_test.jl")
-
+    include("distances/multisets/matching_distances_test.jl")
 
 
 end
