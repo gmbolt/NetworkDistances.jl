090e8ee945d7d264ba6f41ac5f54517eab5d1dfe
George Bolt <g.bolt@lancaster.ac.uk>
2023-07-31 15:44:05 +0100

    Alter how include is called

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,8 @@ using Test
 
 @testset "NetworkDistances.jl" begin
 
-    include("./distances/multisets/matching_distances_test.jl")
+
+    include("distances/multisets/matching_distances_test.jl")
 
 
 end
